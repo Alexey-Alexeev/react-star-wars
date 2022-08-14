3c30361da6739e22a7d174306a798d205978af47
Alexey-Alexeev <alexeevv-alexey@yandex.ru>
2022-08-14 18:39:58 +0300

    add files

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,10 @@
+import PeoplePage from '../PeoplePage/PeoplePage';
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <PeoplePage />
+  )
+}
+
+export default App;
---
+++ src/services/getPeopleData.js
@@ -0,0 +1,27 @@
+import { 
+  HTTP, HTTPS, SWAPI_ROOT, SWAPI_PEOPLE, 
+  GUIDE_IMG_EXTENSION, URL_IMG_PERSON 
+} from '../constants/api';
+
+//-----------------------------------------------
+// Проверка протокола: HTTP или HTTPS
+//-----------------------------------------------
+const checkProtocol = url => {
+    if (url.indexOf(HTTPS) !== -1) {
+        return HTTPS;
+    }
+
+    return HTTP;
+}
+
+const getId = (url, category) => {
+  const protocol = checkProtocol(url);
+  const id = url
+                .replace(protocol+SWAPI_ROOT+category, '')
+                .replace(/\//g,'');
+  return id;
+}
+
+export const getPeopleId = (url) => getId(url, SWAPI_PEOPLE);
+
+export const gerPeopleImage = (id) => `${URL_IMG_PERSON}/${id+GUIDE_IMG_EXTENSION}`
